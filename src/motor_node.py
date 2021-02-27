3c02a769666e0da981a4236e4aa98159558c4e45
Jiri Dohnalek <jiri@groundwow.com>
2021-02-27 19:23:33 +0000

    Update motor_node.py

--- src/motor_node.py
+++ src/motor_node.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 import time
 import rospy
-
-from exomy.msg import MotorCommands
+from exomy.msg import MotorCommands, RobotHeadCommand
 from motors import Motors
 
+
 motors = Motors()
+
 global watchdog_timer
 
 
 def callback(cmds):
+    rospy.loginfo("Moving motors {} to position {}".format(cmds.motor_angles, cmds.motor_speeds))
     motors.setSteering(cmds.motor_angles)
     motors.setDriving(cmds.motor_speeds)
 
@@ -19,6 +21,15 @@ def callback(cmds):
     # then watchdog() is called stopping the driving motors.
     watchdog_timer = rospy.Timer(rospy.Duration(5.0), watchdog, oneshot=True)
 
+
+def head_callback(msg):
+    axis = 'X'
+    if not msg.axis:
+        axis = 'Y'
+
+    motors.move_head(msg.axis, msg.position)
+    rospy.loginfo("Moving head {} to position {}".format(axis, msg.position))
+    
 
 def shutdown():
     motors.stopMotors()
@@ -38,9 +49,8 @@ if __name__ == "__main__":
     global watchdog_timer
     watchdog_timer = rospy.Timer(rospy.Duration(1.0), watchdog, oneshot=True)
 
-    sub = rospy.Subscriber(
-        "/motor_commands", MotorCommands, callback, queue_size=1)
-
+    sub = rospy.Subscriber("/motor_commands", MotorCommands, callback, queue_size=1)
+    rospy.Subscriber("/robot_head", RobotHeadCommand, head_callback, queue_size=1)
+    
     rate = rospy.Rate(10)
-
     rospy.spin()
